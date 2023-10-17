7ab5cf316f6db1524eb707a9305ca32dac942508
shaunstummer <7850615+shaunstummer@users.noreply.github.com>
2023-10-17 11:17:44 +1100

    Add project files.

---
+++ MedicalPrac/Components/AppointmentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MedicalPrac.Components
+{
+    public class AppointmentModel
+    {
+        [Required(ErrorMessage = "Patient Name is required.")]
+        public string PatientName { get; set; }
+
+        [Required(ErrorMessage = "Appointment Date is required.")]
+        public DateTime AppointmentDate { get; set; }
+    }
+}
---
+++ MedicalPrac/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalPrac
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
